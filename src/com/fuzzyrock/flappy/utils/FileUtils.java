9f2c4d8b8946a16aae2532119f73a71d0710a792
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2020-10-30 03:07:42 +0900

    Add Shader class

---
+++ src/com/fuzzyrock/flappy/utils/FileUtils.java
@@ -0,0 +1,27 @@
+package com.fuzzyrock.flappy.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileUtils {
+    private FileUtils() {
+
+    }
+
+    public static String loadAsString(String file) {
+        StringBuilder result = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            reader.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+}
